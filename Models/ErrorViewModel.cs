0fee8c26724544176ae08bbfd3a90535d5152379
morteify <weglarz.michal@outlook.com>
2020-10-24 20:58:13 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace animated_spoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
